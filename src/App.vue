5b279dbb157932baa995d9410edc67e9ffe65ed3
arnaudouttier <arnoutti@gmail.com>
2022-03-29 17:40:05 +0200

    first master init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main id="main">
+    <CountdownTimer />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CountdownTimer from './components/CountdownTimer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CountdownTimer
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: $primary_font;
 }
 </style>

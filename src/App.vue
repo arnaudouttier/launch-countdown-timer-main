8382ad19a0b15e57d6977326788a1105a6e89cd8
arnaudouttier <arnoutti@gmail.com>
2022-03-30 18:25:11 +0200

    nlle beta

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,53 @@
 <template>
   <main id="main">
-    <CountdownTimer />
+    <section class="main-header">
+      <CountdownTimer />
+    </section>
+    <section class="main-footer">
+      <CounterSocialNavigation />
+    </section>
   </main>
 </template>
 
 <script>
 import CountdownTimer from './components/CountdownTimer.vue'
+import CounterSocialNavigation from './components/CounterSocialNavigation.vue'
 
 export default {
   name: 'App',
   components: {
-    CountdownTimer
+    CountdownTimer,
+    CounterSocialNavigation
   }
 }
 </script>
 
 <style lang="scss">
-#app {
+main {
   font-family: $primary_font;
+  height: 100vh;
+  width: 100%;
+   background: linear-gradient(180deg, #1e1f29 80%, #2f2439 20%);
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 3fr 1fr;
+}
+
+.main-header,
+.main-footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.main-header {
+  background-image: url("~@/assets/images/bg-stars.svg");
+  background-repeat: no-repeat;
+}
+
+.main-footer {
+  background-image: url("~@/assets/images/pattern-hills.svg");
+  background-position: 28% top;
+  background-repeat: no-repeat;
 }
 </style>
